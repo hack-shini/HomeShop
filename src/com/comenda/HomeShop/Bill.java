ccdd229dbf3fc85b771954d16cbb32cd3a51e48a
Dimitri comenda <comenda.dimitri007@gmail.com>
2018-09-27 21:42:30 +0200

    Ajout classe Bill (Facture)

--- src/com/comenda/HomeShop/Bill.java
+++ src/com/comenda/HomeShop/Bill.java
@@ -1,7 +1,29 @@
 package com.comenda.HomeShop;
 
+import java.util.Map;
+
 public class Bill {
 
-    Customer customer;
+    private Customer customer;
 
+    private Map<Product, Integer> products;
+
+    /**
+     * Add product with quantity in a Bill
+     * @param product product to add
+     * @param quantity quantity of product
+     */
+
+    public void addProduct(Product product, Integer quantity){
+        this.products.put(product, quantity);
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public Map<Product, Integer> getProducts() {
+        return products;
+    }
 }
+
