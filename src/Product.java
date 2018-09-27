c2757a00956dcb5fd7bdffa7f0271ad8ae560276
Dimitri comenda <comenda.dimitri007@gmail.com>
2018-09-27 09:18:03 +0200

    création classe Product

---
+++ src/Product.java
@@ -0,0 +1,15 @@
+public class Product {
+
+    String name;
+    String description;
+    double price;
+
+    public void look(){
+
+    }
+
+    public void buy (Bill bill, double price){
+
+    }
+
+}
